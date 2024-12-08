48822c1a6a4d941be31713f8720820b0f6491c40
Camilodesenvolvedor <camiloamorim236@gmail.com>
2024-12-08 09:08:17 -0300

    Adding classes generated from StarUML

---
+++ code/Player.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class player(Entity):
+    def __init__(self):
+        pass
+
+    def update(self, ):
+        pass
+
+    def move(self, ):
+        pass
